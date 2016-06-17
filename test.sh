38f930c0a151ba499ecea790d10f98ab1bd5a15a
Daniel Choi <dhchoi@gmail.com>
2016-06-16 23:18:01 -0400

    end of line test comments

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 printf "%-30s%-30s%-30s%-10s\n" INPUT EXPECTED RESULT PASS
-cat tests.txt | grep -v '^#' | grep -v '^ *$' | awk -F ' *:: *' -v OFS="\t" '{$1=$1; print}'| 
+cat tests.txt | grep -v '^#' | grep -v '^ *$' | 
+sed -e 's/ *#.*$//' |
+awk -F ' *:: *' -v OFS="\t" '{$1=$1; print}' | 
 while IFS=$'\t' read a b
 do
   res=$(echo -n "$a" | sphinxesc | sed -e 's/[ \t]*$//' -e 's/^[ \t]*//' )
