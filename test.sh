bc21757af4d40c895b06510986ddb2733a4ed1c3
bobgru <bobgru@hotmail.com>
2016-09-21 07:43:27 -0400

    Fix test.sh on OS X
    The OS X version of sed doesn't recognize '\t' as a tab character.

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ sed -e 's/ *#.*$//' -e 's/ *$//' |              # clear comments and whitespace
 awk -F ' *:: *' -v OFS="\t" '{$1=$1; print}' |  # turn :: field separator into tab
 while IFS=$'\t' read a b
 do
-  res=$(echo -n "$a" | sphinxesc | sed -e 's/[ \t]*$//' -e 's/^[ \t]*//' )
+  res=$(echo -n "$a" | sphinxesc | tr '\t' ' ' | sed -e 's/[ ]*$//' -e 's/^[ ]*//' )
   pass=$( if [ "$res" = "$b" ]; then echo PASS; else  echo '****FAIL***'; fi )
 
   printf "%-30s%-30s%-30s%-10s\n" "$a" "$b" "$res" "$pass"
